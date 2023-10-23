6615b6c928d3d6b3209f58521f2a2f51cb2bb11d
Kartik <kartikcodes44@gmail.com>
2023-10-23 23:17:33 +0530

    email functionality added

--- index.js
+++ index.js
@@ -1,55 +1,100 @@
-const express = require('express')
-const app =express()
-const mongoose = require('mongoose')
-const cors = require('cors');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
-const User = require('./models/user');
-const Post = require('./models/posts');
-const Comment = require('./models/comments');
+const email = "kartikajmera678@hotmail.com";
+const password = "kartik@123";
 
-mongoose.connect('mongodb+srv://kartikajmera890:kartik123456@anchors.1jtf6q1.mongodb.net/?retryWrites=true&w=majority')
-.then(()=>{
-    console.log('Mongoose Running')
-})
-.catch((e)=>{
+const User = require("./models/user");
+const Post = require("./models/posts");
+const Comment = require("./models/comments");
+
+const DB_URL = "mongodb://127.0.0.1:27017/"; //mongodb+srv://kartikajmera890:kartik123456@anchors.1jtf6q1.mongodb.net/?retryWrites=true&w=majority
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Mongoose Running");
+  })
+  .catch((e) => {
     console.log(e);
-})
+  });
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/login',(req,res)=>{
-    const user = new User(req.body)
-    user.save()
-    res.json()
-})
+const transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  auth: {
+    user: email,
+    pass: password,
+  },
+});
 
-app.post('/createPost' , async(req,res)=>{
-    const {content} = req.body
-    const {name , email } = req.body
-    const user = await User.findOne({name ,email})
-    const post = await new Post({content})
-    post.user = user
-    post.save()
-    res.json()
-})
-app.get('/posts' , async(req,res)=>{
-    const posts = await Post.find().populate('user').populate('comments');
-    console.log(posts)
-    res.json(posts) 
-})
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error.message);
+  } else {
+    console.log("ready for message");
+  }
+});
 
-app.post('/addComment' , async(req,res)=>{
-    const {comment , name , email , id} = req.body
-    // const user = await User.findOne({name ,email})
-    const post = await Post.findById(id)
-    const newComment = new Comment({comment})
-    await newComment.save()
-    post.comments.push(newComment) 
-    await post.save()
-    console.log(post)
-    res.json()
-})
-app.listen(3000,()=>{
-    console.log('Listening')
-})
+const sendEmail = async (mailOptions) => {
+  try {
+    await transporter.sendMail(mailOptions);
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.post("/otp", async (req, res) => {
+  console.log(req.body);
+  const { email: formEmail } = req.body;
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  const mailOptions = {
+    from: email,
+    to: formEmail,
+    subject: "Otp",
+    html: `${otp}`,
+  };
+  await sendEmail(mailOptions);
+  res.json(otp);
+});
+
+app.post("/login", (req, res) => {
+  const user = new User(req.body);
+  user.save();
+  res.json();
+});
+
+app.post("/createPost", async (req, res) => {
+  const { content } = req.body;
+  const { name, email } = req.body;
+  const user = await User.findOne({ name, email });
+  const post = await new Post({ content });
+  post.user = user;
+  post.save();
+  res.json();
+});
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find().populate("user").populate("comments");
+  res.json(posts);
+});
+
+app.post("/addComment", async (req, res) => {
+  const { comment, name, email, id } = req.body;
+  // const user = await User.findOne({name ,email})
+  const post = await Post.findById(id);
+  const newComment = new Comment({ comment });
+  await newComment.save();
+  post.comments.push(newComment);
+  await post.save();
+  console.log(post);
+  res.json();
+});
+app.listen(3000, () => {
+  console.log("Listening");
+});
